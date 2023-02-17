84a605577acf667b29f5932fcd0a193ba1851d18
noncomplex <71818162+noncomplex@users.noreply.github.com>
2023-02-17 07:30:51 -0500

    update

--- ncx-theme.el
+++ ncx-theme.el
@@ -1,20 +1,18 @@
 ;; alts: Iosevka Comfy Liberation Mono, Roboto Mono, Consolas, Cousine
 ;; (set-frame-font "Liberation Mono-14" nil t) 
-(set-face-attribute 'default nil :font "Roboto Mono-15:weight=book")
+(set-face-attribute 'default nil :font "Cousine-15:weight=normal")
 (setq initial-frame-alist '((fullscreen . maximized) (font . "Roboto Mono-15:weight=book"))
       default-frame-alist '((fullscreen . maximized) (font . "Roboto Mono-15:weight=book")))
 
-(setq fg-color "#A6C6B6")
+(setq fg-color "#c3c0b0") ;;a6c6b6
 (setq xanadu "#738678")
 (setq mystic "#D65282")
-(set-background-color "#263244")
-(set-background-color "#262832")
+(set-background-color "#3C3F4F") ;;262832
 (set-foreground-color fg-color)
-
 (set-cursor-color fg-color)
 
 (set-face-foreground 'font-lock-keyword-face mystic)
-(set-face-foreground 'font-lock-type-face "#C2A2C2")
+(set-face-foreground 'font-lock-type-face "#2EC09C")
 (set-face-foreground 'font-lock-builtin-face "#2EC09C")
 (set-face-foreground 'font-lock-constant-face "#d1b897")
 (set-face-foreground 'font-lock-function-name-face fg-color)
